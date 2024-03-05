8bbb0a6c1f04f1f5ee758ad738427e403f8ef4c6
alex.curtis14 <alex.curtis14@gmail.com>
2024-03-05 08:12:28 -0500

    initial commit

---
+++ src/main/java/com/alexc/gameimportservice/GameImportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexc.gameimportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameImportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameImportServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexc/gameimportservice/GameImportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexc.gameimportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameImportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
